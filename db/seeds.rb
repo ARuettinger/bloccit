db67b1fa7b7c7f89340a52e795107db09fad325b
Aaron Ruettinger <aaron.ruettinger@gmail.com>
2015-10-01 12:32:17 -0400

    COmpleted assignment 33

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,24 @@
 include RandomData
 
 # Create Posts
-50.times do
-	Post.create!(
-		title: RandomData.random_sentence,
-		body: RandomData.random_paragraph
-		)
-end
-posts = Post.all
+# 50.times do
+# 	Post.create!(
+# 		title: RandomData.random_sentence,
+# 		body: RandomData.random_paragraph
+# 		)
+# end
+# posts = Post.all
 
-# Create Comments
-100.times do 
-	Comment.create!(
-		post: posts.sample,
-		body: RandomData.random_paragraph
-		)
-end
+# # Create Comments
+# 100.times do 
+# 	Comment.create!(
+# 		post: posts.sample,
+# 		body: RandomData.random_paragraph
+# 		)
+# end
 
+p = Post.find_or_create_by(title:"unique title", body:"this is a unique body")
+c = Comment.find_or_create_by(post: p, body: "THis is the new body..the best body of them all.")
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
