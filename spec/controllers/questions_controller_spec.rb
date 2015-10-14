fa03bea034283a975ed45e339ebd9df843340ee0
Aaron Ruettinger <aaron.ruettinger@gmail.com>
2015-10-14 18:13:30 -0400

    Completed assignment 36

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+include RandomData
+
+RSpec.describe QuestionController, type: :controller do
+let(:my_question) {Question.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false) }
+
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_question] to @questions" do
+      get :index
+      expect(assigns(:questions)).to eq([my_question])
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_question.id}
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+      get :show, {id: my_question.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_question to @question" do
+      get :show, {id: my_question.id}
+      expect(assigns(:question)).to eq(my_question)
+    end
+  end
+
+ describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @question" do
+      get :new
+      expect(assigns(:question)).not_to be_nil
+    end
+  end
+
+  describe "QUESTION create" do
+    it "increases the number of the Question by 1" do
+      expect{question :create, question: {title.RandomData.random_sentence, body: RandomData.random_paragraph, resolved:true}}.to change(Question,:count).by(1)
+    end
+
+    it "assigns the new question to @question" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: random_paragraph}}
+      expect(assigns(:question)).to eq Question.last
+    end
+
+    it "redirects to the new question" do
+      post :create, question: {title: RandomData.random_sentence, body: random_paragraph}
+      expect(response).to redirect_to Question.last
+    end
+  end
+#
+#  describe "GET #edit" do
+#    it "returns http success" do
+#      get :edit
+#      expect(response).to have_http_status(:success)
+#    end
+#  end
+
+end
