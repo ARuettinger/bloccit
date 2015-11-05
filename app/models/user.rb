dc37306c67ad000abe8564b2c273416f23f0b486
Aaron Ruettinger <aaron.ruettinger@gmail.com>
2015-11-05 11:44:15 -0500

    Completed checkpoint 40

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :posts
+
 	before_save { self.email = email.downcase }
+	before_save { self.role ||= :member }
+
 	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 	validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 	validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
 	validates :password, length: { minimum: 6 }, allow_blank: true
@@ -10,5 +14,8 @@ class User < ActiveRecord::Base
 						uniqueness: { case_sensitive: false },
 						length: { minimum: 3, maximum: 100 },
 						format: { with: EMAIL_REGEX }
+						
 	has_secure_password
+
+	enum role: [:member, :admin]
 end
