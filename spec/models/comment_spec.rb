22ed276de8cb0ba9762daeebcc50532f73109415
Aaron Ruettinger <aaron.ruettinger@gmail.com>
2015-11-12 22:42:14 -0500

    Completed checkpoint 41

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe Comment do
   	let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)  }
   	let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
+  	it { should belong_to(:post) }
+  	it { should belong_to(:user) }
+  	it { should validate_presence_of(:body) }
+  	it { should validate_length_of(:body).is_at_least(5) }
+
 
   	it "should respond to body" do
   		expect(comment).to respond_to(:body)
